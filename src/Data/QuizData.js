f76ded9f76533efa4b408f7a7460336b4c0daa33
srushti italiya <120362023+srushtiitaliya@users.noreply.github.com>
2023-07-01 08:31:38 +0530

    Add files via upload

---
+++ src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question: "Which of the following is used in React.js to increase performance?",
+        options:["Virtual DOM","Original DOM","Both A and B","None of the above"],
+        answer: 1
+    },
+    {
+        question: "What is ReactJS?",
+        options:["Server-side framework","user interface framework","both a and b","None of the above"],
+        answer: 2
+    },
+    {
+        question: "In which language is React.js written?",
+        options: ["python","javascript","java","php"],
+        answer: 2
+    },
+    {
+        question: " What does props stand for?",
+        options: ["Proper Arguments","Proper Return Values","Properties","All of the above"],
+        answer:3
+    },
+];
